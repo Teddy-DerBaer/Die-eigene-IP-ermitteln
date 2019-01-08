0067f81654bcdb5c3fc75a5ece7b5715fb6e4a1d
Balthasar Cronos <balthasarcronos@hotmail.de>
2019-01-08 20:55:51 +0100

    Es wird die eigene IP ermittelt und angezeigt diese kann auch in die Zwischenablage kopiert werden per Bouton. Bonus: Kurze separate Funktion mit drin, die die IP als IPAddress zurückgibt für Csharp Konsolenanwendungen.

--- Die eigene IP ermitteln/Form1.Designer.cs
+++ Die eigene IP ermitteln/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_ip_aktual = new System.Windows.Forms.Button();
+            this.lbl_anzge = new System.Windows.Forms.Label();
+            this.btn_Kopie_ablge = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_ip_aktual
+            // 
+            this.btn_ip_aktual.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_ip_aktual.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ip_aktual.Location = new System.Drawing.Point(12, 12);
+            this.btn_ip_aktual.Name = "btn_ip_aktual";
+            this.btn_ip_aktual.Size = new System.Drawing.Size(150, 35);
+            this.btn_ip_aktual.TabIndex = 0;
+            this.btn_ip_aktual.Text = "IP Aktualisieren";
+            this.btn_ip_aktual.UseVisualStyleBackColor = true;
+            this.btn_ip_aktual.Click += new System.EventHandler(this.Btn_ip_aktual_Click);
+            // 
+            // lbl_anzge
+            // 
+            this.lbl_anzge.AutoSize = true;
+            this.lbl_anzge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_anzge.Location = new System.Drawing.Point(185, 19);
+            this.lbl_anzge.Name = "lbl_anzge";
+            this.lbl_anzge.Size = new System.Drawing.Size(100, 20);
+            this.lbl_anzge.TabIndex = 1;
+            this.lbl_anzge.Text = "IP-Anzeige";
+            // 
+            // btn_Kopie_ablge
+            // 
+            this.btn_Kopie_ablge.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Kopie_ablge.Enabled = false;
+            this.btn_Kopie_ablge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Kopie_ablge.Location = new System.Drawing.Point(12, 63);
+            this.btn_Kopie_ablge.Name = "btn_Kopie_ablge";
+            this.btn_Kopie_ablge.Size = new System.Drawing.Size(242, 35);
+            this.btn_Kopie_ablge.TabIndex = 3;
+            this.btn_Kopie_ablge.Text = "IP Kopieren->ZwAblge. ";
+            this.btn_Kopie_ablge.UseVisualStyleBackColor = true;
+            this.btn_Kopie_ablge.Click += new System.EventHandler(this.Btn_Kopie_ablge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(384, 124);
+            this.Controls.Add(this.btn_Kopie_ablge);
+            this.Controls.Add(this.lbl_anzge);
+            this.Controls.Add(this.btn_ip_aktual);
+            this.Name = "Form1";
+            this.Text = "Meine IP ist ?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_ip_aktual;
+        private System.Windows.Forms.Label lbl_anzge;
+        private System.Windows.Forms.Button btn_Kopie_ablge;
     }
 }
 
--- Die eigene IP ermitteln/Form1.cs
+++ Die eigene IP ermitteln/Form1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
 
 namespace Die_eigene_IP_ermitteln
 {
@@ -15,5 +12,93 @@ namespace Die_eigene_IP_ermitteln
         {
             InitializeComponent();
         }
+
+        private void Btn_ip_aktual_Click(object sender, EventArgs e)
+        {
+            string ip = "";
+            string time_anzg = "";
+            string url = "http://api.ipify.org";
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                using (HttpWebResponse res = (HttpWebResponse)req.GetResponse())
+                {
+                    using (StreamReader str = new StreamReader(res.GetResponseStream()))
+                    {
+                        ip = str.ReadToEnd();
+                        str.Close();
+                        time_anzg = "Zeit : " + DateTime.Now.ToLocalTime().ToString("dd.MM.yyyy HH:mm ") + "Uhr ";
+                        //wenn ich Zeit habe, mach ich eine Regex Zerlegung in IPv4 und IPv6 Erkennung
+                        // damit man auch andere Seiten aufrufen kann als "http://api.ipify.org"
+                        lbl_anzge.Text = IPAddress.Parse(ip).ToString();
+                        this.Text = "Die IP ..  " + lbl_anzge.Text;
+                        btn_Kopie_ablge.Enabled = true;
+                    }
+                    res.Close();
+                }
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show("Quelle : " + es.Source + "\n\r" + "Botschaft : " + es.Message, "Eine Ausnahme erwischt!!!");
+            }
+        }
+
+        private void Btn_Kopie_ablge_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetDataObject(IPAddress.Parse(lbl_anzge.Text).ToString(), true);
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show("Quelle : " + es.Source + "\n\r" + "Botschaft : " + es.Message, "Eine Ausnahme erwischt!!!");
+            }
+        }
+
+
+        //Ich habe hier das ganze als eine Simple Funktion geschrieben einfach rauskopieren
+
+        //------------Funktion-Anfang---------------------------------
+        public IPAddress My_IP()
+        // public string My_IPstr()  
+        {
+            string ip = "";
+            string url = "http://api.ipify.org";
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                using (HttpWebResponse res = (HttpWebResponse)req.GetResponse())
+                {
+                    using (StreamReader str = new StreamReader(res.GetResponseStream()))
+                    {
+                        ip = str.ReadToEnd();
+                        //wenn ich Zeit habe, mach ich eine Regex Zerlegung in IPv4 und IPv6 Erkennung
+                        // damit man auch andere Seiten aufrufen kann als "http://api.ipify.org"
+                        str.Close();
+                    }
+                    res.Close();
+                }
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show("Quelle : " + es.Source + "\n\r" + "Botschaft : " + es.Message, "Eine Ausnahme erwischt!!!");
+            }
+            try
+            {
+                return IPAddress.Parse(ip);
+                //return IPAddress.Parse(ip).ToString();
+            }
+            catch (Exception es1)
+            {
+                MessageBox.Show("Quelle : " + es1.Source + "\n\r" + "Botschaft : " + es1.Message + "\n\rAchtung : Es wird eine 0.0.0.0 IP zurückgegeben", "Eine Ausnahme erwischt!!!");
+                ip = "0.0.0.0";
+                return IPAddress.Parse(ip);
+                //return IPAddress.Parse(ip).ToString();
+            }
+        }
+
+        //------------------Funktion-Ende-------------------------------
     }
 }
